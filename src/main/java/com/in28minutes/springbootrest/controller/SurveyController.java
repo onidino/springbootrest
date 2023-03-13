c1230ab5a27b0cee4a18a057e34aeac87cddc697
onidino <lucianodefilippe@gmail.com>
2023-03-12 21:57:49 -0300

    Logic for all the GET operations

--- src/main/java/com/in28minutes/springbootrest/controller/SurveyController.java
+++ src/main/java/com/in28minutes/springbootrest/controller/SurveyController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springbootrest.controller;
 
+import com.in28minutes.springbootrest.entity.QuestionDto;
 import com.in28minutes.springbootrest.entity.SurveyDto;
 import com.in28minutes.springbootrest.service.SurveyService;
 import java.util.List;
@@ -44,12 +45,53 @@ public class SurveyController {
   public SurveyDto getSurveyById(
       @PathVariable("id") String surveyId) {
 
-    SurveyDto surveyFound = this.surveyService.getSurveyById(surveyId);
+    SurveyDto surveyFound = surveyService.getSurveyById(surveyId);
     if (surveyFound == null) {
-      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Survey not found");
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          String.format("Survey not found for id [%s]", surveyId));
     }
 
-    return this.surveyService.getSurveyById(surveyId);
+    return surveyFound;
+  }
+
+  /**
+   * Basic endpoint to obtain all the questions from a specific survey.
+   *
+   * @param surveyId the survey id to find.
+   * @return the survey with the given id.
+   */
+  @RequestMapping("/surveys/{survey_id}/questions")
+  public List<QuestionDto> getQuestionsFromSurveyById(
+      @PathVariable("survey_id") String surveyId) {
+    List<QuestionDto> questionsList = surveyService.getQuestionsFromSurveyById(surveyId);
+    if (questionsList.isEmpty()) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          String.format("Questions not found for survey id [%s]", surveyId));
+    }
+
+    return questionsList;
+  }
+
+  /**
+   * Basic endpoint to obtain a specific survey with the id sent in the path.
+   *
+   * @param surveyId   the survey id to find.
+   * @param questionId the question to find by id in the survey.
+   * @return the survey with the given id.
+   */
+  @RequestMapping("/surveys/{survey_id}/question/{question_id}")
+  public QuestionDto getQuestionFromSurveyById(
+      @PathVariable("survey_id") String surveyId,
+      @PathVariable("question_id") String questionId) {
+
+    QuestionDto questionFound = surveyService.getQuestionByIdFromSurveyById(surveyId, questionId);
+    if (questionFound == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+          String.format("Question not found for survey id [%s] and question id [%s]",
+              surveyId, questionId));
+    }
+
+    return questionFound;
   }
 
 }
--- src/main/java/com/in28minutes/springbootrest/service/SurveyService.java
+++ src/main/java/com/in28minutes/springbootrest/service/SurveyService.java
@@ -4,7 +4,9 @@ import com.in28minutes.springbootrest.entity.QuestionDto;
 import com.in28minutes.springbootrest.entity.SurveyDto;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -52,10 +54,51 @@ public class SurveyService {
    *
    * @return a survey
    */
-  public SurveyDto getSurveyById(String id) {
+  public SurveyDto getSurveyById(
+      final String id) {
+
     return SURVEY_LIST.stream()
         .filter(survey -> survey.getId().equalsIgnoreCase(id))
         .findFirst()
+        .orElse(null);
+  }
+
+  /**
+   * Returns a specific question with the id passed as param from the survey id passed as param.
+   *
+   * @param surveyId survey id to find.
+   * @return a list of questions of the survey found.
+   */
+  public List<QuestionDto> getQuestionsFromSurveyById(
+      final String surveyId) {
+
+    Optional<SurveyDto> surveyFound = SURVEY_LIST.stream()
+        .filter(survey -> survey.getId().equalsIgnoreCase(surveyId))
+        .findFirst();
+    if (surveyFound.isPresent()) {
+      return surveyFound.get().getQuestions();
+    }
+
+    return Collections.emptyList();
+  }
+
+  /**
+   * Returns a specific question by id from the survey found by id.
+   *
+   * @param surveyId   survey id to find.
+   * @param questionId question id to find.
+   * @return a question
+   */
+  public QuestionDto getQuestionByIdFromSurveyById(
+      final String surveyId, final String questionId) {
+
+    Optional<SurveyDto> surveyFound = SURVEY_LIST.stream()
+        .filter(survey -> survey.getId().equalsIgnoreCase(surveyId))
+        .findFirst();
+
+    return surveyFound.flatMap(surveyDto -> surveyDto.getQuestions().stream()
+            .filter(question -> question.getId().equalsIgnoreCase(questionId))
+            .findFirst())
         .orElse(null);
   }
 
